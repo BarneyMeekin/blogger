0b8b126cede67088975b0ec088b58ee48a31f525
Barney Meekin <BarneyJMeekin@gmail.com>
2016-10-20 10:37:04 +1300

    done authentcation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161019002652) do
+ActiveRecord::Schema.define(version: 20161019171756) do
 
   create_table "application_records", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 20161019002652) do
   create_table "attachments", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "username",         null: false
+    t.string   "email",            null: false
+    t.string   "crypted_password", null: false
+    t.string   "salt",             null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["email"], name: "index_authors_on_email", unique: true
   end
 
   create_table "comments", force: :cascade do |t|
